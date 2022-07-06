17270df753f040e33107a0061f29455687e8aab9
ivan-shargin <shargin.ia@phystech.edu>
2022-07-06 13:33:23 +0300

    added sahr_only_legs.urdf, added right damp, limits, friction for joints

--- legged_gym/envs/sahr/sahr_config.py
+++ legged_gym/envs/sahr/sahr_config.py
@@ -5,8 +5,8 @@ from isaacgym import gymapi
 class SahrRoughCfg( LeggedRobotCfg ):
     class env( LeggedRobotCfg.env ):
         num_envs = 1024
-        num_actions = 20
-        num_observations = 72
+        num_actions = 12
+        num_observations = 48
         episode_length_s = 20.0
     class terrain( LeggedRobotCfg.terrain ):
         mesh_type = 'plane' 
@@ -40,7 +40,7 @@ class SahrRoughCfg( LeggedRobotCfg ):
 
             'left_shoulder_pitch': 0.0,
             'left_shoulder_roll': 0.0,
-            'left_elbow': 0.0,
+            'left_elbow': 0.0
         }
 
 
@@ -61,20 +61,20 @@ class SahrRoughCfg( LeggedRobotCfg ):
             'left_shoulder_pitch': 20.0,
             'left_shoulder_roll': 20.0,
             'left_elbow': 20.0}
-        damping = { 'left_knee': 0.5, 'left_hip_yaw': 0.5,
-            'left_hip_roll': 0.5, 'left_hip_pitch': 0.5, 
-            'left_ankle_pitch': 0.5,  'left_ankle_roll': 0.5, 'right_knee': 0.5, 'right_hip_yaw': 0.5,
-            'right_hip_roll': 0.5, 'right_hip_pitch': 0.5, 
-            'right_ankle_pitch': 0.5, 'right_ankle_roll': 0.5, 'head_yaw': 0.5,
-            'head_pitch': 0.5,
+        damping = { 'left_knee': 1.23, 'left_hip_yaw': 0.65,
+            'left_hip_roll': 1.23, 'left_hip_pitch': 1.23, 
+            'left_ankle_pitch': 1.23,  'left_ankle_roll': 1.23, 'right_knee': 1.23, 'right_hip_yaw': 0.65,
+            'right_hip_roll': 1.23, 'right_hip_pitch': 1.23, 
+            'right_ankle_pitch': 1.23, 'right_ankle_roll': 1.23, 'head_yaw': 1.23,
+            'head_pitch': 1.23,
             
-            'right_shoulder_pitch': 0.5,
-            'right_shoulder_roll': 0.5,
-            'right_elbow': 0.5,
+            'right_shoulder_pitch': 0.65,
+            'right_shoulder_roll': 0.65,
+            'right_elbow': 0.65,
 
-            'left_shoulder_pitch': 0.5,
-            'left_shoulder_roll': 0.5,
-            'left_elbow': 0.5 }   # [N*m*s/rad]
+            'left_shoulder_pitch': 0.65,
+            'left_shoulder_roll': 0.65,
+            'left_elbow': 0.65 }   # [N*m*s/rad]
         
         # action scale: target angle = actionScale * action + defaultAngle
         action_scale = 0.5
@@ -82,7 +82,7 @@ class SahrRoughCfg( LeggedRobotCfg ):
         decimation = 4
 
     class asset( LeggedRobotCfg.asset ):
-        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/sahr/urdf/sahr.urdf'
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/sahr/urdf/sahr_only_legs.urdf'
         name = "sahr"
         foot_name = "foot"
         penalize_contacts_on = ["trunk_1", "u_shoulder_1", "u_shoulder_2",
@@ -106,10 +106,10 @@ class SahrRoughCfg( LeggedRobotCfg ):
     class rewards( LeggedRobotCfg.rewards ):
         class scales( LeggedRobotCfg.rewards.scales ):
             # dof_pos_limits = 0.0
-            # lin_vel_z = -0.2
+            lin_vel_z = -3.0
             # tracking_lin_vel = 1.0
             # tracking_ang_vel = 0.5
-            # ang_vel_xy = -0.01
+            ang_vel_xy = -0.01
             # orientation = -0.
             # dof_vel = -0.001
             # dof_acc = -2.5e-7
@@ -117,17 +117,17 @@ class SahrRoughCfg( LeggedRobotCfg ):
             # feet_air_time =  1.0
             # collision = -1.
             # feet_stumble = -0.0 
-            action_rate = -0.0025
+            action_rate = -0.001
             # stand_still = -0.0
-            # termination = -100.0
+           
         
         time_of_step = 0.3 #longer steps get positive reward
-        # soft_dof_pos_limit = 0.95
-        # base_height_target = 0.35        
-        # soft_dof_vel_limit = 0.95
-        # soft_torque_limit = 0.95
-        # max_contact_force = 250.0 # forces above this value are penalized
-        # only_positive_rewards = False
+        soft_dof_pos_limit = 0.99
+        base_height_target = 0.35        
+        soft_dof_vel_limit = 0.95
+        soft_torque_limit = 1.0
+        max_contact_force = 250.0 # forces above this value are penalized
+        only_positive_rewards = False
 
     class sim (LeggedRobotCfg.sim ):
         dt =  0.0025
@@ -138,3 +138,4 @@ class SahrRoughCfgPPO( LeggedRobotCfgPPO ):
     class runner( LeggedRobotCfgPPO.runner ):
         run_name = ''
         experiment_name = 'rough_sahr'
+        # load_run = "Jul05_15-23-43_"
